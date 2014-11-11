0a133ead30b955d7d20d690466bc49ffa5fa1d8e
hjacobs <henning@jacobs1.de>
2014-11-11 23:05:20 +0100

    stuff

--- presence/__main__.py
+++ presence/__main__.py
@@ -19,19 +19,37 @@ MONTHS = {
 }
 
 def main():
+    entries = []
     for fn in glob.glob('/var/log/syslog*'):
         openfun = open
         if fn.endswith('.gz'):
             openfun = gzip.open
+        last_uptime = 0
         with openfun(fn, mode='rt', encoding='utf-8') as fd:
             for line in fd:
+                if line.startswith('\x00'):
+                    continue
+                parts = line.split()
+                mon = MONTHS[parts[0].lower()]
+                now = datetime.datetime.now()
+                hour, minute, second = parts[2].split(':')
+                dt = datetime.datetime(year=now.year, month=mon, day=int(parts[1]), hour=int(hour), minute=int(minute), second=int(second))
                 if 'kernel:' in line:
-                    parts = line.split()
-                    mon = MONTHS[parts[0].lower()]
-                    now = datetime.datetime.now()
-                    hour, minute, second = parts[2].split(':')
-                    dt = datetime.datetime(year=now.year, month=mon, day=int(parts[1]), hour=int(hour), minute=int(minute), second=int(second))
-                    print(dt, parts[6])
+                    last_uptime = float(' '.join(parts[5:(7 if parts[5] == '[' else 6)]).strip(' []'))
+                    entries.append((dt, last_uptime))
+                else:
+                    entries.append((dt, last_uptime))
+
+
+    last_uptime = 0
+    last_ts = None
+    last_start = None
+    for ts, uptime in sorted(entries):
+        if uptime < last_uptime:
+            print(last_start, '--', last_ts, uptime, last_uptime, ts)
+            last_start = ts
+        last_uptime = uptime
+        last_ts = ts
 
 
 if __name__ == '__main__':
