c7dd330c4b70ffaeafbb285ed843bc3ffb6e4e34
Henning Jacobs <henning@jacobs1.de>
2014-11-14 19:29:46 +0100

    use utmp

--- README.rst
+++ README.rst
@@ -1,3 +1,5 @@
 ========
 Presence
 ========
+
+Presence calculates times between machine boots and shutdowns, i.e. it gives the "uptime" over multiple days/reboots.
--- presence/__main__.py
+++ presence/__main__.py
@@ -1,7 +1,7 @@
 
 import datetime
 import glob
-import gzip
+import utmp
 
 MONTHS = {
     'jan': 1,
@@ -18,39 +18,28 @@ MONTHS = {
     'dec': 12
 }
 
+
 def main():
     entries = []
-    for fn in glob.glob('/var/log/syslog*'):
-        openfun = open
-        if fn.endswith('.gz'):
-            openfun = gzip.open
-        last_uptime = 0
-        with openfun(fn, mode='rt', encoding='utf-8') as fd:
-            for line in fd:
-                if line.startswith('\x00'):
-                    continue
-                parts = line.split()
-                mon = MONTHS[parts[0].lower()]
-                now = datetime.datetime.now()
-                hour, minute, second = parts[2].split(':')
-                dt = datetime.datetime(year=now.year, month=mon, day=int(parts[1]), hour=int(hour), minute=int(minute), second=int(second))
-                if 'kernel:' in line:
-                    last_uptime = float(' '.join(parts[5:(7 if parts[5] == '[' else 6)]).strip(' []'))
-                    entries.append((dt, last_uptime))
-                else:
-                    entries.append((dt, last_uptime))
+    for fn in glob.glob('/var/log/wtmp*'):
+        with open(fn, 'rb') as fd:
+            for entry in utmp.read(fd.read()):
+                if entry.user in ['reboot', 'shutdown']:
+                    entries.append(entry)
 
+    entries.sort(key=lambda x: x.time)
 
-    last_uptime = 0
-    last_ts = None
-    last_start = None
-    for ts, uptime in sorted(entries):
-        if uptime < last_uptime:
-            print(last_start, '--', last_ts, uptime, last_uptime, ts)
-            last_start = ts
-        last_uptime = uptime
-        last_ts = ts
+    last_reboot = None
+    for entry in entries:
+        if entry.user == 'shutdown' and last_reboot:
+            print(last_reboot, entry.time - last_reboot)
+            last_reboot = None
+        elif entry.user == 'reboot':
+            last_reboot = entry.time
 
+    if last_reboot:
+        now = datetime.datetime.now()
+        print(last_reboot, now - last_reboot)
 
 if __name__ == '__main__':
     main()
